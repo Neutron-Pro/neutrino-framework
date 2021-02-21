060a6c09d9a15287a3bde3e117b54a363d156a7a
Neutron-Pro <neutron@neutronstars.fr>
2021-02-21 13:29:26 +0100

    Add page contact example

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,8 +2,12 @@
 namespace App\Controller;
 
 
+use NeutronStars\Form\Bootstrap\EmailElement;
+use NeutronStars\Form\Bootstrap\SubmitElement;
+use NeutronStars\Form\Bootstrap\TextAreaElement;
 use NeutronStars\Form\Bootstrap\TextElement;
 use NeutronStars\Form\Bootstrap\XSRFElement;
+use NeutronStars\Form\Form;
 use NeutronStars\Neutrino\Core\Controller;
 
 class HomeController extends Controller
@@ -15,15 +19,40 @@ class HomeController extends Controller
 
     public function contact(): void
     {
-        $form = $this->createForm($_POST, '', 'POST', new XSRFElement());
+        $form = $this->createFormContact($_POST);
 
-        $form->add(new TextElement('Nom *', 'name', 'name', [
-            'min' => '3',
-            'max' => '32'
-        ], 'Entrez votre nom'));
+        if ($form->isSubmit() && $form->isValid()) {
+            $this->createEmail()
+                 ->add($form->get('email'), $form->get('name'))
+                 ->send($form->get('subject'), 'mail.contact', [
+                     'name'    => $form->get('name'),
+                     'message' => $form->get('message')
+                 ]);
+            $success = 'Votre message à bien été envoyé.';
+            $form = $this->createFormContact();
+        }
 
         $this->render('app.contact', [
-            'form' => $form
+            'form'    => $form,
+            'success' => $success ?? null
         ]);
+    }
+
+    private function createFormContact(array $values = []): Form
+    {
+        return ($this->createForm($values, '', 'POST', new XSRFElement()))
+            ->add(new TextElement('Nom *', 'name', 'name', [
+                'min' => 3,
+                'max' => 32
+            ], 'Entrez votre nom'))
+            ->add(new EmailElement('Email *', 'email', 'email'))
+            ->add(new TextElement('Sujet *', 'subject', 'subject', [
+                'min' => 3,
+                'max' => 32
+            ], 'Quel est le sujet de votre message ?'))
+            ->add(new TextAreaElement('Message *', 'message', 'message', [
+                'min' => 10
+            ], 'Tapez votre message...'))
+            ->add(new SubmitElement('Envoyer le message'));
     }
 }
