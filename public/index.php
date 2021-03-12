3d96df97b9141c0bb9cb2994138faae8c5a07abe
Neutron-Pro <neutron@neutronstars.fr>
2021-03-12 14:56:53 +0100

    Fix Router Param GET into URI

--- public/index.php
+++ public/index.php
@@ -5,11 +5,12 @@ use NeutronStars\Neutrino\Core\Kernel;
 use NeutronStars\Router\Router;
 require_once '../vendor/autoload.php';
 $configuration = new Configuration('../config/config.json');
+
 Kernel::create(
     $configuration,
     new Router(
         $configuration->get('domainUrl', 'http://127.0.0.1'),
-        $_SERVER['REQUEST_URI'] ?? '404',
+        str_replace('?'.($_SERVER['QUERY_STRING'] ?? ''), '', $_SERVER['REQUEST_URI'] ?? '404'),
         $configuration->get('baseUrl', '')
     )
 );
